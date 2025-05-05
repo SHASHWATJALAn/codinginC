55a0e81d63eee2b9ea2e071b740264e52cbea76d
SHASHWAT JALAn <shasvatawesome44@gmail.com>
2025-05-05 12:57:23 +0530

    Made a program to find max of 3 numbersv in c

---
+++ maxofthreE.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter 3 numbers a,b,c\n");
+    scanf("%d",a);
+    scanf("%d",b);
+    scanf("%d",c);
+    if(a>b){
+        if(a>c){
+            printf("First number is greatest\n");
+            printf("%d",a);
+        }
+        else{
+            printf("Third number is greatest\n");
+            printf("%d",c);
+        }
+
+
+    }
+    else 
+    {
+if(b>c)
+{
+printf("Second number is greatest\n");
+printf("%d",b);
+}
+else
+{
+printf("Third number is greatest\n");
+printf("%d",c);
+}
+
+    }
+    
+
+    
+}
